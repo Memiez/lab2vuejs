9ce801bd553d218b4dc9a059ca850fe724c7a898
Memiez <iam_minny@hotmail.com>
2022-12-02 16:28:05 +0700

    Finished Hello world exsample

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,13 @@
 import { RouterLink, RouterView } from "vue-router";
 import HelloWorld from "./components/HelloWorld.vue";
 import CalComponents from "./components/CalComponents.vue";
+import HelloWorldExsample from "./components/CalComponents.vue";
 </script>
 
 <template>
   <div>
-    <CalComponents></CalComponents>
-    <!--ดึงมาวางซ้ำกันหลายตัว ใส่แค่
-  <CalComponents />
-  <CalComponents />
-  <CalComponents />-->
+    <CalComponents />
+    <HelloWorldExsample />
   </div>
 </template>
 
