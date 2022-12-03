ba2e9dcba59c3830d82e401ce8e9ecbdee6ef821
Memiez <iam_minny@hotmail.com>
2022-12-03 16:37:26 +0700

    Handing User Input

--- src/components/HandingUserInput.vue
+++ src/components/HandingUserInput.vue
@@ -4,13 +4,18 @@ const message = ref("Hello World!");
 function reverseMessage() {
   message.value = message.value.split("").reverse().join("");
 }
+function notify() {
+  alert("navigation was prevented");
+}
 </script>
 
 <template>
   <h1>{{ message }}</h1>
   <button @click="reverseMessage">Reverse Message</button>
   <button v-on:click="message += '!'">Append "!"</button>
-  <a href="">A link with e.preventDefault</a>
+  <a href="https://vuejs.org" @click.prevent="notify"
+    >A link with e.preventDefault</a
+  >
 </template>
 
 <style scoped>
