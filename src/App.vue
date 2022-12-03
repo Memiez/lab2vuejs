7cf09dcf4eebbd822b7cc9d8fd2106cabcffa784
Memiez <iam_minny@hotmail.com>
2022-12-03 16:20:45 +0700

    SplitReverseJoin

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,13 @@
 import { RouterLink, RouterView } from "vue-router";
 import HelloWorld from "./components/HelloWorld.vue";
 import CalComponents from "./components/CalComponents.vue";
-import HelloWorldExsample from "./components/CalComponents.vue";
+import HelloWorldExsample from "./components/HelloWorldExsample.vue";
+import HandingUserInput from "./components/HandingUserInput.vue";
 </script>
 
 <template>
   <div>
+    <HandingUserInput />
     <CalComponents />
     <HelloWorldExsample />
   </div>
---
+++ src/components/HandingUserInput.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { ref } from "vue";
+const message = ref("Hello World!");
+function reverseMessage() {
+  message.value = message.value.split("").reverse().join("");
+}
+</script>
+
+<template>
+  <h1>{{ message }}</h1>
+  <button @click="reverseMessage">Reverse Message</button>
+  <button v-on:click="message += '!'">Append "!"</button>
+  <a href="">A link with e.preventDefault</a>
+</template>
+
+<style scoped>
+button,
+a {
+  display: block;
+  margin-bottom: 1em;
+}
+</style>
