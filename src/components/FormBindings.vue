0a824c1de005c1416b7dfa164cabb5e64a839ab9
Memiez <iam_minny@hotmail.com>
2022-12-08 14:26:55 +0700

    Radio

--- src/components/FormBindings.vue
+++ src/components/FormBindings.vue
@@ -3,6 +3,7 @@ import { ref } from "vue";
 const text = ref("Edit Me");
 const checked = ref(true);
 const checkedNames = ref(["Memo"]);
+const picked = ref("One");
 </script>
 
 <template>
@@ -21,6 +22,12 @@ const checkedNames = ref(["Memo"]);
   <input type="checkbox" v-model="checkedNames" id="์Nadej" value="Nadej" />
   <label for="Nadej">Nadej</label>
   <p>Checked names</p>
-  <pre> {{checkedNames}} </pre>
-  
+  <pre> {{ checkedNames }} </pre>
+
+  <h2>Radio</h2>
+  <input type="radio" id="one" value="One" v-model="picked" />
+  <label for="one">One </label>
+  <input type="radio" id="two" value="Two" v-model="picked" />
+  <label for="two">Two </label> <br />
+  <span>Picked {{ picked }}</span>
 </template>
